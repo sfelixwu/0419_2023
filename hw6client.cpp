fc0e58b5511cfef5a4f1f7475288068e4d04fa66
sfelixwu <wu@cs.ucdavis.edu>
2023-04-19 17:21:20 -0700

    Add files via upload

---
+++ hw6client.cpp
@@ -0,0 +1,34 @@
+
+// ecs36b first program
+
+#include <iostream>
+#include "ecs36b_Exception.h"
+
+// JSON RPC part
+#include "hw6client.h"
+#include <jsonrpccpp/client/connectors/httpclient.h>
+
+using namespace jsonrpc;
+using namespace std;
+
+int
+main()
+{
+  HttpClient httpclient("http://127.0.0.1:8384");
+  hw6Client myClient(httpclient, JSONRPC_CLIENT_V2);
+  Json::Value myv;
+  Json::Value jv_list;
+  
+  jv_list[0] = "987654321";
+  jv_list[1] = "987654322";
+  jv_list[2] = "987654323";
+
+  //
+  try {
+    myv = myClient.set_name("set_name", jv_list, "hi5555");
+  } catch (JsonRpcException &e) {
+    cerr << e.what() << endl;
+  }
+  std::cout << myv.toStyledString() << std::endl;
+  return 0;
+}
